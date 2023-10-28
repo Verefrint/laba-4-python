28da81af4422a79453dcdfc59254e726ef94cabf
Verefrint <DimaVe2004@gmail.com>
2023-10-28 14:10:58 +0300

    second comm

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def read_html_file(filename):
         html_string = f.read()
     return html_string
 
-
+def parse_all_emails(html_string):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.findall(pattern, html_string)
 
 html_string = read_html_file('example.html')
 print("html file:")
